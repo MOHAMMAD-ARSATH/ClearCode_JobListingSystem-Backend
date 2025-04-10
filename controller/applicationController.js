d591c75747045f420be8ca2bff3d7db5924da695
Mohammad Arsath <92676635+MOHAMMAD-ARSATH@users.noreply.github.com>
2025-04-10 22:01:44 +0530

    applicationController.js

--- controller/applicationController.js
+++ controller/applicationController.js
@@ -30,7 +30,7 @@ exports.applyJob = async (req, res) => {
         name,
         email,
         contact,
-        gender,
+        gender; gender || null,
         address,
         resume: `${API_URL}/uploads/${resumeFile}`,
         coverLetter: `${API_URL}/uploads/${coverLetterFile}` || null,
