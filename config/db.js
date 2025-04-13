58149af62e8fb37501b141233512b1c6ef3c9a26
Mohammad Arsath <arsathmd11@gmail.com>
2025-04-13 21:43:08 +0530

    first commit

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const mongoURL = process.env.MONGO_URL;
 
 mongoose
   .connect(mongoURL, { useUnifiedTopology: true, useNewUrlParser: true })
-  .then(() => console.log("✅ MongoDB Connection Successful"))
+  .then(() => console.log("✅ MongoDB Connection Successfully"))
   .catch((err) => console.log("❌ MongoDB Connection Failed:", err));
 
 module.exports = mongoose;
