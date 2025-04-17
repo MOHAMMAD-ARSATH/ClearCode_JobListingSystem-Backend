7c45de5ef6b61ab2c5aac6ff0287d367e853eedf
Mohammad Arsath <92676635+MOHAMMAD-ARSATH@users.noreply.github.com>
2025-04-17 22:31:53 +0530

    server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
 
 require("./config/db");
 
@@ -9,8 +10,10 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({
-  origin: ["https://clear-code-jobs.vercel.app"],
+  origin: ["http://localhost:3000", "https://clear-code-jobs.vercel.app"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type"]
