458977078f0988e36319f4003cb123becb326995
Mohammad Arsath <92676635+MOHAMMAD-ARSATH@users.noreply.github.com>
2025-04-17 22:54:41 +0530

    server.js

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({
   origin: ["http://localhost:3000", "https://clear-code-jobs.vercel.app"],
   credentials: true,
