8b558a68438ec9b26060f12190799b08a2abad3c
Mohammad Arsath <92676635+MOHAMMAD-ARSATH@users.noreply.github.com>
2025-04-18 21:40:31 +0530

    server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const path = require("path");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
-// const fileUpload = require('express-fileupload');
+const fileUpload = require('express-fileupload');
 
 require("./config/db");
 
@@ -11,8 +11,8 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({
   origin: ["http://localhost:3000", "https://clear-code-jobs.vercel.app"],
   credentials: true,
@@ -20,7 +20,7 @@ app.use(cors({
   allowedHeaders: ["Content-Type"]
 }));
 app.use(morgan("dev"));
-// app.use(fileUpload());
+app.use(fileUpload());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // Routes
